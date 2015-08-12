8d22f69f73aa7ce5b6177338229b99f02cc099f0
Diego Martins <dondiego7@gmail.com>
2015-08-12 14:14:58 -0300

    creating as a function

--- js/mappingEvents.js
+++ js/mappingEvents.js
@@ -1,16 +1,18 @@
 var mappedEvents = {};
 
-$('*').each(function() {
-
-	var thisEvent = jQuery._data($(this).get(0), "events");
-
-	if (thisEvent) {
-		$.each(thisEvent, function(attr, value) {
-				if (mappedEvents[attr]) {
-					mappedEvents[attr] = mappedEvents[attr] + 1
-				} else {
-					mappedEvents[attr] = 1
-				}
-		});
-	} 
-});
+function mapAllEvents() {
+	$('*').each(function() {
+	
+		var thisEvent = jQuery._data($(this).get(0), "events");
+	
+		if (thisEvent) {
+			$.each(thisEvent, function(attr, value) {
+					if (mappedEvents[attr]) {
+						mappedEvents[attr] = mappedEvents[attr] + 1
+					} else {
+						mappedEvents[attr] = 1
+					}
+			});
+		} 
+	});
+}
