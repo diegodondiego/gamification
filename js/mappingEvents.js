6910e5e4a2a0b32d53795c3ea1eb56ff97cf41de
Diego Martins <dondiego7@gmail.com>
2015-08-12 16:31:06 -0300

    refactoring and evaluate css on HTML
    still need to evaluate CSS's files

--- js/mappingEvents.js
+++ js/mappingEvents.js
@@ -1,9 +1,10 @@
 var mappedEvents = {};
 
 function mapAllEvents() {
+	// find all events using jquery
 	$('*').each(function() {
-	
-		var thisEvent = jQuery._data($(this).get(0), "events");
+
+		var thisEvent = $._data($(this)[0], 'events');
 	
 		if (thisEvent) {
 			$.each(thisEvent, function(attr, value) {
@@ -13,6 +14,24 @@ function mapAllEvents() {
 						mappedEvents[attr] = 1
 					}
 			});
-		} 
+		}	
+		
 	});
+	
+	// evaluate the css of the page for hover actions.
+	var styles = document.styleSheets;
+	for(var i=0, len=styles.length; i < len; i++) {
+
+	    var sheet = styles[i], rules = sheet.cssRules;
+	    if (rules) {
+		    for(var j=0, len2=rules.length; j<len;j++) {
+		    
+		        var rule = rules[j];
+		        if(rule && rule.selectorText == "a:hover"){
+					mappedEvents['mouseout']++;
+					mappedEvents['mouseover']++;
+				}
+		    }
+	    }
+	}
 }
