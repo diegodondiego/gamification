134287cdecc9d7056e57bf1e25a5f14fe7a04432
Diego Martins <dondiego7@gmail.com>
2015-08-12 11:58:11 -0300

    first release

--- js/mappingEvents.js
+++ js/mappingEvents.js
@@ -1,7 +1,16 @@
-var mappedEvents;
+var mappedEvents = {};
 
 $('*').each(function() {
 
-	var thisEvent = jQuery._data($(this)[0], "events"));
-	
-}
+	var thisEvent = jQuery._data($(this)[0], "events");
+
+	if (thisEvent) {
+		$.each(thisEvent, function(attr, value) {
+				if (mappedEvents[attr]) {
+					mappedEvents[attr] = mappedEvents[attr] + 1
+				} else {
+					mappedEvents[attr] = 1
+				}
+		});
+	} 
+});
